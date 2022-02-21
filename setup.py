25eeb9d526a50c79c5d26f70e65cb35234dd9c65
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-02-21 18:36:08 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ setup(
   package_dir = {'genes':'codons'},
   packages = find_packages(),
   package_data = {
-	'codons':['rosetta_stone/*', 'test/*']
+	'codons':['rosetta_stone/*'],
+    'test': ['*']
   },
-  version = '0.0.5',
+  version = '0.0.6',
   license = 'MIT',
   description = "Translates and transcribes an arbitrary genetic sequence, generates FASTA-formatted files, and interfaces with BLAST databases to identify genetic and protein sequences.", 
   long_description = readme,
